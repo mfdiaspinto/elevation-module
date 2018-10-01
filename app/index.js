d82c08d15d6cd7f9c11a248d7a987f0fec243d17
mfdiaspinto <mfdiaspinto@gmail.com>
2018-10-01 11:54:55 +0100

    s

--- app/index.js
+++ app/index.js
@@ -29,7 +29,7 @@ module.exports = class extends Generator {
     );
 
     this.fs.copyTpl(
-      this.templatePath('src/**'),
+      this.templatePath('src/.*'),
       this.destinationPath(this.answers.module + '/src'),
 			{ module: this.answers.module,
 				author :  this.answers.author,
